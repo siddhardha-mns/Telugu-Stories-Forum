81b7b7e54f481768171174e5f5bb8407dd146285
MADAGALA NIKHIL SAI SIDDHARDHA <siddhardhamns@gmail.com>
2025-07-20 14:17:06 +0530

    Creating basic interface

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# --- Page Configuration ---
+st.set_page_config(
+    page_title="తెలుగు కథలు",
+    page_icon="📖",
+    layout="centered"
+)
+
+# --- Header ---
+st.title("📖 తెలుగు కథలు")
+st.markdown("పంచుకోండి, చదవండి, చర్చించండి.")
